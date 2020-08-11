6934d9cef9ec51f5a31589b23279bfb1a19d2e78
NightScript <maorninja@outlook.com>
2020-08-11 15:10:20 -0700

    Remove ARGV precursors

--- arm9/source/romloader_frontend.c
+++ arm9/source/romloader_frontend.c
@@ -96,52 +96,6 @@ void SplitItemFromFullPathAlias(const char *pFullPathAlias,char *pPathAlias,char
 	if(pFilenameAlias)strcpy(pFilenameAlias,&pFullPathAlias[SplitPos]);
 }
 
-bool _readFrontend(char *target){
-	FILE *f=fopen("/loadfile.dat","rb");
-	if(f){
-		int i=0;
-		myfgets((char*)buf,768,f);
-		fclose(f);
-		unlink("/loadfile.dat");
-		//if(!memcmp((char*)buf,"fat:",4))i+=4;
-		if(!memcmp((char*)buf+i,"//",2))i+=1;
-		if(!memcmp((char*)buf+i,"/./",3))i+=2; //menudo dir handling is buggy?
-		strcpy(target,(char*)buf+i);
-		if(strlen(target)<4||(strcasecmp(target+strlen(target)-4,".nes")&&strcasecmp(target+strlen(target)-4,".fds")))return false;
-		return true;
-	}
-	f=fopen("/plgargs.dat","rb");
-	if(f){
-		int i=0;
-		myfgets((char*)buf,768,f);
-		myfgets((char*)buf,768,f); //second line
-		fclose(f);
-		unlink("/plgargs.dat");
-		//if(!memcmp((char*)buf,"fat:",4))i+=4;
-		//if(!memcmp((char*)buf+i,"//",2))i+=1;
-		//if(!memcmp((char*)buf+i,"/./",3))i+=2;
-		strcpy(target,(char*)buf+i);
-		if(strlen(target)<4||(strcasecmp(target+strlen(target)-4,".nes")&&strcasecmp(target+strlen(target)-4,".fds")))return false;
-		return true;
-	}
-	f=fopen("/moonshl2/extlink.dat","r+b");
-	if(f){
-		TExtLinkBody extlink;
-		memset(&extlink,0,sizeof(TExtLinkBody));
-		fread(&extlink,1,sizeof(TExtLinkBody),f);
-		if(extlink.ID!=ExtLinkBody_ID){fclose(f);return false;}
-		//strcpy(target,extlink.DataFullPathFilenameAlias);
-		ucs2tombs((unsigned char*)target,extlink.DataFullPathFilenameUnicode,768);
-		fseek(f,0,SEEK_SET);
-		fwrite("____",1,4,f);
-		fflush(f);
-		fclose(f);
-		if(strlen(target)<4||(strcasecmp(target+strlen(target)-4,".nes")&&strcasecmp(target+strlen(target)-4,".fds")))return false;
-		return true;
-	}
-	return false; //it is your choice to boot GUI or to halt.
-}
-
 extern int argc;
 extern char **argv;
 bool readFrontend(char *target)
@@ -150,7 +104,7 @@ bool readFrontend(char *target)
 	if(argc>1){
 		strcpy(target,argv[1]);
 	}else{
-		if(!_readFrontend(target))return false;
+		return false;
 	}
 	SplitItemFromFullPathAlias(target,dir,NULL);
 	chdir(dir);
