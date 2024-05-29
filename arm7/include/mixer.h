44ce7f8e4367a4e8641f37e00cc169237ad08f7d
Dartz150 <davidsoriam92@gmail.com>
2024-05-29 15:51:27 -0600

    Adjusted souns frequency a bit lower than the Max the DS can output - Fixes issues with certain APU VCR6 channel sounds.

--- arm7/include/mixer.h
+++ arm7/include/mixer.h
@@ -12,20 +12,20 @@ extern "C" {
 #define MIXBUFSIZE                      128
 
 // DS output Frequency after mixing is 32.768 kHz 10 bits, this should be equal or below.
-#define MIXFREQ                        (32768)
+#define MIXFREQ                        (32700)
 
 // DSi output Frequency after mixing is 47.609 kHz 10 bits, this should be equal or below.
-#define DSI_MIXFREQ                    (32768)  //(47609)
+#define DSI_MIXFREQ                    (32700)  //(47609)
 
 #define NES_MIXFREQ                    (24064)
 #define TEST_MIXFREQ                   (65536)
 
 // FROM GBATEK: Nintendo DS Audio Bus Clock Frequency = 33.513982MHz/2
-#define BUS_CLOCK                      (33513982)  
+#define DS_BUS_CLOCK                   (33513982)  
 #define DSI_BUS_CLOCK                  (33513982) //(47609000)
 
 // Proper rounding formula by "Asiekierka" of BlocksDS team https://github.com/blocksds/libnds/pull/49
-#define TIMER_FREQ_SHIFT(n, divisor, shift) ((-((BUS_CLOCK >> (shift)) * (divisor)) - ((((n) + 1)) >> 1)) / (n))
+#define TIMER_FREQ_SHIFT(n, divisor, shift) ((-((DS_BUS_CLOCK >> (shift)) * (divisor)) - ((((n) + 1)) >> 1)) / (n))
 #define DSI_TIMER_FREQ_SHIFT(n, divisor, shift) ((-((DSI_BUS_CLOCK >> (shift)) * (divisor)) - ((((n) + 1)) >> 1)) / (n))
 
 enum
