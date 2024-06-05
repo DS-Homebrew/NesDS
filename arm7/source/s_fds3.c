3f0d8c95252960607108bc112db215cd219d3a4c
Dartz150 <davidsoriam92@gmail.com>
2024-06-04 23:23:58 -0600

    Update Division Frequency fix for FDS sound

--- arm7/source/s_fds3.c
+++ arm7/source/s_fds3.c
@@ -280,11 +280,13 @@ static NES_READ_HANDLER s_fds_read_handler[] =
 	{ 0,      0,      0, },
 };*/
 
+//TODO: MOVE DIVFIX to a common code file
 static Uint32 DivFix(Uint32 p1, Uint32 p2, Uint32 fix)
 {
 	Uint32 ret;
 	ret = p1 / p2;
-	p1  = p1 % p2;/* p1 = p1 - p2 * ret; */
+	p1  = p1 % p2;
+	//p1 = p1 - p2 * ret;
 	while (fix--)
 	{
 		p1 += p1;
