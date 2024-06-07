484a37be2eef147b2e9eb3dd23f8d0388e84a342
Dartz150 <davidsoriam92@gmail.com>
2024-06-06 19:42:44 -0600

    Increased Audio Mixer Max Buffer size to avoid glitchy audio - Fixes both VRC6 and FDS games sound issues. - Partially fixes VRC6 romhacks sound issues.

--- arm7/include/mixer.h
+++ arm7/include/mixer.h
@@ -9,10 +9,10 @@ extern "C" {
 #endif
 
 // Buffer for PCM samples
-#define MIXBUFSIZE                      128
+#define MIXBUFSIZE                      512
 
 // DS output Frequency after mixing is 32.768 kHz 10 bits, this should be equal or below.
-#define MIXFREQ                        (32768) //32768 // 32000
+#define MIXFREQ                        (32768) //32700 // 32000
 
 //Temporary Define for tests, RAW PCM Channel only works fine with this frequency for some unknown reason
 #define RAW_PCM_FREQ                   (24064) //Matches the NES cycle freq
